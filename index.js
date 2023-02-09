cf3b8a8362e2a2a965087f58f5a3feb2c1b11ddc
Ritika Mishra <103934960+Ritika8081@users.noreply.github.com>
2023-02-09 10:20:07 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM  from "react-dom/client";
+import App from './App'
+import './index.css'
+
+
+ ReactDOM.createRoot(
+   document.getElementById("root")).render(<>
+    <h1 className="heading_style">LIST OF SOME WEB-SERIES </h1>
+   
+    <App/>
+   
+   </>,
+ )
