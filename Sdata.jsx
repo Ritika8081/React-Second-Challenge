2dcb38a73c4fb67b2a49570db23500824a03332b
Ritika Mishra <103934960+Ritika8081@users.noreply.github.com>
2023-02-09 10:20:54 +0530

    Add files via upload

---
+++ Sdata.jsx
@@ -0,0 +1,25 @@
+const Sdata =[
+{
+  id:1,
+    imgsrc:"https://i.ytimg.com/an_webp/DGgG2EJ3Pe4/mqdefault_6s.webp?du=3000&sqp=CJaHip8G&rs=AOn4CLCmkzRF2I_5oDPwrrAuMMyyPF6o1A",
+  title:"Original series",
+  sname:"When You Meet Your First Love ",
+  link:"https://www.youtube.com/watch?v=DGgG2EJ3Pe4",
+},
+{
+  id:2,
+  imgsrc:"https://i.ytimg.com/an_webp/Ev4GOeRcUVU/mqdefault_6s.webp?du=3000&sqp=COOpip8G&rs=AOn4CLAJZ3ErTKujtyu4RR9pnvhbGRuOEw",
+  title:"Original series",
+  sname:"Long Drive With Ex & Crush ",
+  link:"https://www.youtube.com/watch?v=Ev4GOeRcUVU",
+
+},
+{
+  id:3,
+  imgsrc:"https://i.ytimg.com/an_webp/SG6zLrbAngk/mqdefault_6s.webp?du=3000&sqp=CPyQip8G&rs=AOn4CLBrrI9utljhr7mGc_mLlPwtYfCyTQ",
+  title:"Original series",
+  sname:"The Interns ",
+  link:"https://www.youtube.com/watch?v=SG6zLrbAngk",
+}
+]
+export default Sdata;
